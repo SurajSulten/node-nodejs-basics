1aa0f55928008c2c70f6241d7d900bd63756d01c
Suroj <suraj.sulten@gmail.com>
2024-02-02 21:46:51 +0500

    do remaning tasks

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,21 @@
 const parseEnv = () => {
-    // Write your code here 
-};
+    const { execSync } = require('child_process');
+
+        const commandOutput = execSync('printenv').toString();
+        const envVariables = commandOutput.split('\n');
+
+        const rssVariables = {};
+        envVariables.forEach(variable => {
+            if (variable.startsWith("RSS_")) {
+                const [key, value] = variable.split('=');
+                rssVariables[key] = value;
+            }
+        });
+
+        for (const key in rssVariables) {
+            console.log(`RSS_${key}=${rssVariables[key]};`);
+        }
+
+    };
 
 parseEnv();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,13 @@
 const decompress = async () => {
-    // Write your code here 
+    const fs = require('fs');
+    const zlib = require('zlib');
+
+    const readStream = fs.createReadStream('archive.gz');
+    const gunzip = zlib.createGunzip();
+    const writeStream = fs.createWriteStream('fileToCompress.txt');
+
+    readStream.pipe(gunzip).pipe(writeStream);
+
 };
 
 await decompress();
